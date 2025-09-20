9ccc31d591865032592ff8c602079bae6cc31ebb
Érick da silva <eierickdasilva@gmail.com>
2025-09-20 20:42:43 -0300

    feat: adding basic password system

--- projects/sirne/src/app/app.routes.ts
+++ projects/sirne/src/app/app.routes.ts
@@ -2,10 +2,32 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Settings } from './pages/settings/settings';
 import { Avatar } from './pages/avatar/avatar';
+import { Login } from './pages/login/login';
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-    { path: 'home', component: Home },
-    { path: 'settings', component: Settings },
-    { path: 'avatar', component: Avatar },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+    {
+        path: 'home',
+        component: Home,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'login',
+        component: Login,
+    },
+    {
+        path: 'settings',
+        component: Settings,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'avatar',
+        component: Avatar,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
 ];
---
+++ projects/sirne/src/app/pages/login/login.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Auth } from 'cerebellum';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  public loginSel: Auth = { appid: '', appkey: '', icon: '', name: 'Estudante' };
+
+  constructor(
+    private router: Router
+  ) {}
+
+  public login() {
+    localStorage.setItem('auth', JSON.stringify(this.loginSel));
+    this.router.navigate(['home']);
+  }
+}
