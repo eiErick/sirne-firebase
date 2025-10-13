84ecea6936b599c109cecbccd4e80d544a76e859
Iago <iagomenezes555@gmail.com>
2025-10-13 16:53:51 -0300

    add: button to select own avatar

--- projects/sirne/src/app/pages/avatar/avatar.ts
+++ projects/sirne/src/app/pages/avatar/avatar.ts
@@ -6,23 +6,20 @@ import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-avatar',
-  imports: [
-    CommonModule,
-    HeaderComponent,
-    RandomAvatar
-  ],
+  imports: [CommonModule, HeaderComponent, RandomAvatar],
   templateUrl: './avatar.html',
-  styleUrl: './avatar.scss'
+  styleUrl: './avatar.scss',
 })
 export class Avatar implements OnInit, OnDestroy {
   public auth = computed(() => this.settingsService.auth());
   public avatars: string[];
   private backButtonListener: any;
+  public img = '';
 
   constructor(
     private location: Location,
     private settingsService: SettingsService,
-    private snackbar: SnackbarService,
+    private snackbar: SnackbarService
   ) {
     this.avatars = this.settingsService.avatars;
   }
@@ -55,5 +52,23 @@ export class Avatar implements OnInit, OnDestroy {
     this.settingsService.setIcon(path);
     this.avatarRandom(false);
     this.snackbar.showSuccess('Avatar selecionado com sucesso!');
+  }
+
+  fileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files || input.files.length === 0) return;
+
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.img = reader.result as string;
+      localStorage.setItem('imagem', this.img);
+      this.settingsService.setIcon(this.img);
+      this.avatarRandom(false);
+      this.snackbar.showSuccess('Avatar selecionado com sucesso!');
+    };
+
+    reader.readAsDataURL(file);
   }
 }
