c0e302ca2a69105d5742ae08a217f85fbe950c3e
Érick da silva <eierickdasilva@gmail.com>
2025-10-18 09:51:01 -0300

    fix: fixing statusbar

--- projects/sirne/src/app/app.ts
+++ projects/sirne/src/app/app.ts
@@ -1,8 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StatusBar, Style } from '@capacitor/status-bar';
+import { EdgeToEdge } from '@capawesome/capacitor-android-edge-to-edge-support';
 
-StatusBar.setOverlaysWebView({ overlay: false });
+const enableEdgeToEdge = async () => {
+  await EdgeToEdge.enable();
+};
+
+enableEdgeToEdge();
 
 @Component({
   selector: 'app-root',
@@ -10,7 +15,7 @@ StatusBar.setOverlaysWebView({ overlay: false });
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('sirne');
 
   public darkMode = false;
@@ -19,28 +24,69 @@ export class App {
     this.initializeApp();
   }
 
-  public initializeApp() {
+  public async initializeApp() {
     this.initTheme();
+    await StatusBar.setOverlaysWebView({ overlay: false });
   }
 
   private initTheme() {
     const storedTheme = localStorage.getItem('dark-mode');
+    const storedOrange = localStorage.getItem('deep-orange-mode');
 
+    let deepOrange: boolean = false;
+
+    if (storedOrange) {
+      deepOrange = JSON.parse(storedOrange);
+
+    }
+    
+    if (deepOrange) {
+      document.documentElement.classList.add('app-deep-orange');
+    } else {
+      document.documentElement.classList.add('app-orange');
+    }
+    
     if (storedTheme) {
       const theme = JSON.parse(storedTheme);
 
       if (theme) {
         document.documentElement.classList.add('app-dark');
-        StatusBar.setBackgroundColor({ color: '#ff6600' });
-        StatusBar.setStyle({ style: Style.Dark });
+
+        if (deepOrange) {
+          EdgeToEdge.setBackgroundColor({ color: '#ff6600' });
+          StatusBar.setBackgroundColor({ color: '#ff6600' });
+          StatusBar.setStyle({ style: Style.Dark });
+        } else {
+          EdgeToEdge.setBackgroundColor({ color: '#16161A' });
+          StatusBar.setBackgroundColor({ color: '#16161A' });
+          StatusBar.setStyle({ style: Style.Dark });
+        }
       } else {
         document.documentElement.classList.add('app-light');
-        StatusBar.setBackgroundColor({ color: '#ff6600' });
-        StatusBar.setStyle({ style: Style.Dark });
+
+        if (deepOrange) {
+          EdgeToEdge.setBackgroundColor({ color: '#ff6600' });
+          StatusBar.setBackgroundColor({ color: '#ff6600' });
+          StatusBar.setStyle({ style: Style.Dark });
+        } else {
+          EdgeToEdge.setBackgroundColor({ color: '#EAEAEC' });
+          StatusBar.setBackgroundColor({ color: '#EAEAEC' });
+          StatusBar.setStyle({ style: Style.Light });
+        }
       }
     } else {
       document.documentElement.classList.add('app-light');
       localStorage.setItem('dark-mode', 'false');
+
+      if (deepOrange) {
+        EdgeToEdge.setBackgroundColor({ color: '#ff6600' });
+        StatusBar.setBackgroundColor({ color: '#ff6600' });
+        StatusBar.setStyle({ style: Style.Dark });
+      } else {
+        EdgeToEdge.setBackgroundColor({ color: '#EAEAEC' });
+        StatusBar.setBackgroundColor({ color: '#EAEAEC' });
+        StatusBar.setStyle({ style: Style.Light });
+      }
     }
   }
 }
