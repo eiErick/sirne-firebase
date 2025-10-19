47abe2b4645711df12f3acdd43fb7ecffa1fb4c9
Iago <iagomenezes555@gmail.com>
2025-10-19 12:17:17 -0300

    add: button for your own avatar on maraskin and sirne

--- projects/maraskin/src/app/app.config.ts
+++ projects/maraskin/src/app/app.config.ts
@@ -6,7 +6,7 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { envirionment } from '../../../../environments/environment';
+import { envirionment } from '../../../../environments/environment'
 
 export const appConfig: ApplicationConfig = {
   providers: [
