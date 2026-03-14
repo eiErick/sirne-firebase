a5e2a9fe25c915102272e28aa95ddd947634e1c4
Érick da silva <eierickdasilva@gmail.com>
2026-03-14 19:52:44 -0300

    feat: add convertSimpleToMenu()

--- projects/cerebellum/src/lib/services/menu/menu.service.ts
+++ projects/cerebellum/src/lib/services/menu/menu.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { addDoc, collection, collectionData, deleteDoc, doc, docData, Firestore, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { Menu, MenuRequest, MenuResponse } from '../../models/menu.model';
+import { MealResponse, MealViewModel, Menu, MenuRequest, MenuResponse, SimpleDayCell } from '../../models/menu.model';
+import { MealService } from '../meal/meal.service';
+import { Mapper } from '../../utils/Model.util';
 
 export interface Item {
   id?: string;
@@ -21,7 +23,8 @@ export class MenuService {
   private itemsCollection;
 
   constructor(
-    private firestore: Firestore
+    private firestore: Firestore,
+    private mealService: MealService,
   ) {
     this.itemsCollection = collection(this.firestore, 'menus');
   }
@@ -32,6 +35,39 @@ export class MenuService {
 
   public getItems(): Observable<MenuResponse[]> {
     return collectionData(this.itemsCollection, { idField: 'id' }) as Observable<Menu[]>
+  }
+
+  public convertSimpleToMenu(simpleDayMenu: SimpleDayCell): Menu {
+    const { snacks, lunches } = this.mealService.getStorage();
+
+    console.log(snacks);
+    
+
+    const menu: Menu = { day: simpleDayMenu.day, id: '', lunches: [], snacks: [], today: false }
+
+    for (const snack of simpleDayMenu.snacks) {
+      const found = snacks.find((s) => s.id === snack.id);
+
+      if (!!found) {
+        const snackView = Mapper.mapMatchingProperties(snack, new MealViewModel());
+        menu.snacks.push(snackView);
+      }
+    }
+
+    for (const lunch of simpleDayMenu.lunches) {
+      const found = lunches.find((s) => s.id === lunch.id);
+
+      if (!!found) {
+        const snackView = Mapper.mapMatchingProperties(lunch, new MealViewModel());
+        menu.lunches.push(snackView);
+      }
+    }
+
+    return menu;
+  }
+
+  public addWeek() {
+
   }
 
   public getMealId(id: number) {
